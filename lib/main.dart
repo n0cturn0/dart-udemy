d99c50c4a5940df5f485f292ae0aca0f8728e9a6
Luiz Augusto da S. Oliveira <loliveira@PSG.LOCAL>
2024-07-26 16:36:20 -0400

    Adicionado AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
+@override
 class PerguntaApp extends StatelessWidget {
   Widget build(BuildContext contex) {
     return MaterialApp(
-      home: Text('ERROR CODE FAIL2!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Code cool'),
+      ),
     );
   }
 }
