304643a9e2bb9ed60ef93f55e52db6592b53530d
Luiz Augusto da S. Oliveira <loliveira@PSG.LOCAL>
2024-07-26 15:30:46 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext contex) {
+    return MaterialApp(
+      home: Text('ERROR CODE FAIL2!'),
+    );
+  }
+}
