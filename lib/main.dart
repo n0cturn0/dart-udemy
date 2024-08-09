19018567462d78d1372a8c88e84f72e9fa93b120
Luiz Augusto da S. Oliveira <loliveira@PSG.LOCAL>
2024-08-09 11:21:09 -0400

    Váriaveis statelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,41 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-@override
 class PerguntaApp extends StatelessWidget {
+  final questionSelected = 0;
+  void responder() {
+    questionSelected++;
+    print(questionSelected);
+  }
+
+  @override
   Widget build(BuildContext contex) {
+    final perguntas = [
+      'What is your favorite color?',
+      'What is your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Code cool'),
+        body: Column(
+          children: [
+            Text(perguntas[questionSelected]),
+            ElevatedButton(
+              child: Text('Response One'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Response two'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Response thre'),
+              onPressed: responder,
+            )
+          ],
+        ),
       ),
     );
   }
